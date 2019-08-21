6d66342347aeead53b7b44ee3929e838ba113d73
Qasim Rizvi <syed.rizvi2385@gmail.com>
2019-08-22 01:01:27 +0500

    added DB connection with server

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,5 @@
+// In order to connect database run this command in seperate terminal or window
+// 'sudo service mongod restart'
 // database url
 module.exports = {
     url: 'mongodb://localhost:27017/easy-notes'
--- server.js
+++ server.js
@@ -11,6 +11,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
+/* DataBase configuration start */
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connection the DataBase
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+})
+    .then(() => {
+        console.log('Successfully connected to DataBase');
+    })
+    .catch((err) => {
+        console.log('Could not connect to DataBase. Exiting now...', err);
+        process.exit();
+    });
+
+/* DataBase configuration end */
+
 // define a base route
 app.get('/', (req, res) => {
     res.json({ 'message': "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
