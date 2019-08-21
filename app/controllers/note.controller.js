485bf3cc0d1ff0c484fcdf651d1ed770a2e109f7
Qasim Rizvi <syed.rizvi2385@gmail.com>
2019-08-22 01:19:33 +0500

    connect routes with server

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,27 @@
+// import note model
+const Note = require('../models/note.model.js');
+
+// Create and Save a new Note
+exports.Create = (req, res) => {
+    // creation code goes here
+};
+
+// Retreiving and retrun all notes from the database
+exports.findAll = (req, res) => {
+    // code goes here
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+    // code goes here
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+    // code goes here
+};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+    // code goes here
+};
