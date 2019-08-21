6627e59d2299ae15cdd02e9e497b27c3278d2530
Qasim Rizvi <syed.rizvi2385@gmail.com>
2019-08-22 01:30:40 +0500

    added create and fetch controllers

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -3,17 +3,65 @@ const Note = require('../models/note.model.js');
 
 // Create and Save a new Note
 exports.Create = (req, res) => {
-    // creation code goes here
+    // Vaidate request
+    if (!req.body.content) {
+        return res.status(400).send({
+            message: 'Note can not be emoty',
+        });
+    }
+
+    // Create a Note
+    const note = new Note({
+        title: req.body.title || 'Untitled Note',
+        content: req.body.content
+    });
+
+    // Save note in database
+    note.save()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while create new note',
+            });
+        });
 };
 
 // Retreiving and retrun all notes from the database
 exports.findAll = (req, res) => {
-    // code goes here
+    Note.find()
+        .then(notes => {
+            res.send(notes);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occourred while retreiving notes.'
+            });
+        });
 };
 
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
-    // code goes here
+    Note.findById(req.params.noteId)
+        .then(note => {
+            if (!note) {
+                return res.status(404).send({
+                    message: 'Note not found with id' + req.params.noteId,
+                });
+            }
+            res.send(note);
+        })
+        .catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving note with id " + req.params.noteId
+            });
+        });
 };
 
 // Update a note identified by the noteId in the request
